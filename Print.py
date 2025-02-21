ad6b806c3251e263bcb521089507f5360f815d2b
mamu2119 <chaitanyapandya1985@gmail.com>
2025-02-21 21:05:47 +0530

    Add files via upload

---
+++ Print.py
@@ -0,0 +1,5 @@
+# printing a greeting message
+print("Hello, world! How are you all?")
+print("My name is Marmik.")
+print("I love solving rubik's cube.i.e.2x2x2,3x3x3,4x4x4 and pyramid.")
+print("The result of 11 + 22 is:", 11 + 22)
